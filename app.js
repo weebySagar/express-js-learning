cbe4eb460da8fa60aeee2d3d35d00aa1d2cc659d
Sagar Moriya <sagar.moriya@digiwhiz.com.au>
2023-08-31 21:20:08 +0530

    intro to middlewares

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use((req,res,next)=>{
-console.log('In the middleware');
-next();
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input name="title" type="text"><input name="size" type="number"><button type="submit">submit</button></form>')
 });
 
-app.use((req,res)=>{
-    console.log('In another middleware');
+app.post('/product',(req,res)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/',(req,res)=>{
     res.send('<h1>hello from node js</h1>')
 })
 
