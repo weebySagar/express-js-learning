d8495c61464d1b0e651025903e845ffd04769019
Sagar Moriya <sagar.moriya@digiwhiz.com.au>
2023-09-04 22:07:50 +0530

    dynamic routes part 3

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,7 +6,8 @@ exports.getAddProduct = (req, res, next) => {
     path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true
+    activeAddProduct: true,
+    editing:false 
   });
 };
 
@@ -15,10 +16,35 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
+
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if(!editMode){
+    return res.redirect('/')
+  }
+  const {productId }= req.params;
+  Product.findById(productId,product=>{
+    if(!product) return res.redirect('/');
+    res.render('admin/add-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing :editMode,
+      product:product
+    });
+  })
+};
+
+exports.postEditProduct =(req,res)=>{
+const {title,productId, imageUrl, price,description} = req.body;
+const updatedProduct = new Product(productId,title,imageUrl,description,price);
+updatedProduct.save();
+res.redirect('/admin/products')
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -29,3 +55,10 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.deleteProduct=(req,res)=>{
+const {productId} = req.params;
+const deleteProduct = Product.delete(productId);
+if(!deleteProduct) console.log('something went wrong');;
+res.redirect('/admin/products')
+}
--- routes/admin.js
+++ routes/admin.js
@@ -9,10 +9,18 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
+// admin/edit-product => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product',adminController.postEditProduct);
+
+
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.post('/delete-product/:productId',adminController.deleteProduct)
 
 module.exports = router;
